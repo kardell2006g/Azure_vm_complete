2bc047b73c1f5e49c50c09b990129f20252e4854
Adam K <50672995+kardell2006g@users.noreply.github.com>
2022-04-21 16:32:42 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,11 +1,9 @@
-Include a backend file to use Terraform Cloud. (backend.tf)
-
 terraform {
   cloud {
     organization = "gekk0"
-  }
+
     workspaces {
-      name = "azure_vm_complete_app"
-    
+      name = "azure_vm_complete"
+    }
   }
 }
