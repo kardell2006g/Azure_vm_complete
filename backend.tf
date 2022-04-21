ecd6241d39572fdba1c75df79904431c2befece3
Adam K <50672995+kardell2006g@users.noreply.github.com>
2022-04-21 14:09:41 -0400

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,11 @@
+Include a backend file to use Terraform Cloud. (backend.tf)
+
+terraform {
+  cloud {
+    organization = "gekk0"
+
+    workspaces {
+      name = "azure_vm_complete_vcs"
+    }
+  }
+}
